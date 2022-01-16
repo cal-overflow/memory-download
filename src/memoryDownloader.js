34f68cff8d5d74db7fd65586ade6bd50d58049f5
Christian Lisle <lisleachristian@gmail.com>
2022-01-16 14:43:15 -0600

    Refactored project and removed child processes

---
+++ src/memoryDownloader.js
@@ -0,0 +1,38 @@
+
+import {
+  downloadPhotos,
+  downloadVideos
+} from './services/downloadServices.js';
+import {
+  initializeEnvironment,
+  getMemoryDataFromJSON,
+  zipFiles
+} from './services/fileServices.js';
+
+const isDebugging = process.env.DEBUG_MODE;
+
+export const downloadMemories = async (socket) => {  
+  initializeEnvironment(socket);
+  
+  const data = getMemoryDataFromJSON(socket.file);
+  
+  if (!data['Saved Media']) {
+    socket.emit('message', {error: 'Unable to parse the file you provided.<br />Please try uploading the <tt>memories_history.json</tt> file again.'});
+    return;
+  }
+  
+  const memories = data['Saved Media'].reverse();
+  socket.emit('message', {total: memories.length});
+
+  if (isDebugging) console.log(`[${socket.id}] Processing ${memories.length} memories`);
+  
+  await downloadPhotos(memories, socket);
+  await downloadVideos(memories, socket);
+  await zipFiles(socket);
+  
+  socket.emit('message', {
+    count: memories.length,
+    isComplete: true,
+    downloadRoute: `archive/${socket.id}/memories.zip`
+  });
+};
