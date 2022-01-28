a4feefebe65a1a2139730b0ec481370ed5ceb18a
Christian Lisle <lisleachristian@gmail.com>
2022-01-27 22:32:03 -0600

    Cleaned up styling of app

--- src/preload.js
+++ src/preload.js
@@ -2,5 +2,4 @@ window.addEventListener('DOMContentLoaded', () => {
   const version = document.getElementById('version');
 
   version.innerHTML = process.env.npm_package_version;
-  version.classList.remove('spinner-border', 'spinner-border-sm');
 });
