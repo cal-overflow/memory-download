e52255ddf2de4ea1ce17c52f930dd30d6229d51f
Abhideep Maity <abhideep.maity@quicksell.co>
2022-05-29 16:08:02 +0530

    delete original file

--- src/services/exif.js
+++ src/services/exif.js
@@ -1,11 +1,13 @@
-const {exiftool} = require('exiftool-vendored');
+const { exiftool } = require('exiftool-vendored');
+const { unlink } = require('fs/promises');
 const dayjs = require('dayjs');
 
 const updateExifData = async (fileName, creationDateTimeString) => {
   const exifFormattedDate = dayjs.utc(creationDateTimeString,'YYYY-MM-DD HH:mm:ss Z').format('YYYY:MM:DD HH:mm:ss');
-  return exiftool.write(fileName, {
+  await exiftool.write(fileName, {
     'DateTimeOriginal': exifFormattedDate
   })
+  await unlink(`${fileName}_original`);
 };
 
 module.exports = {
