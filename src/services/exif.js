2178d5e433c2816690287ee34be5416f70d96ecb
Abhideep Maity <abhideepm@gmail.com>
2022-05-31 22:52:53 +0530

    Add EXIF to photos (#61)
    * add exif helper
    
    * add to .gitignore
    
    * delete original file
    
    * add await and return for mp4 files
    
    * 1.5.1
    
    * add new version to documentation
    
    Co-authored-by: Abhideep Maity <abhideepm@gmail.com>

---
+++ src/services/exif.js
@@ -0,0 +1,22 @@
+const { ExifTool } = require('exiftool-vendored');
+const { unlink } = require('fs/promises');
+const { extname } = require('path')
+const dayjs = require('dayjs');
+
+const exiftool = new ExifTool({});
+
+const updateExifData = async (fileName, creationDateTimeString) => {
+  const extension = extname(fileName);
+  if(extension === '.mp4') { // mp4 files are not supported by exiftool
+    return
+  }
+  const exifFormattedDate = dayjs.utc(creationDateTimeString,'YYYY-MM-DD HH:mm:ss Z').format('YYYY:MM:DD HH:mm:ss');
+  await exiftool.write(fileName, {
+    'DateTimeOriginal': exifFormattedDate
+  })
+  await unlink(`${fileName}_original`);
+};
+
+module.exports = {
+  updateExifData,
+}
