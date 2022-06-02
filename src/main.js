08472d9502da35a8aaf08622b9736d3311c9d963
Christian Lisle <42817415+ChristianLisle@users.noreply.github.com>
2022-06-02 17:39:37 -0500

    🧼 Add ESLint & Prettier (#66)

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-const path = require('path');
-const { app, BrowserWindow, shell, ipcMain, dialog } = require('electron');
-const { downloadMemories } = require('./memoryDownloader');
-const dayjs = require('dayjs');
-const utc = require('dayjs/plugin/utc');
+const path = require("path");
+const { app, BrowserWindow, shell, ipcMain, dialog } = require("electron");
+const { downloadMemories } = require("./memoryDownloader");
+const dayjs = require("dayjs");
+const utc = require("dayjs/plugin/utc");
 
 dayjs.extend(utc);
 
@@ -22,30 +22,29 @@ const createWindow = () => {
     },
   });
 
-  win.loadFile('src/public/index.html');
+  win.loadFile("src/public/index.html");
 
   win.webContents.setWindowOpenHandler(({ url }) => {
-    if (process.platform === 'darwin' && 'file://' === url.substring(0, 7)) {
+    if (process.platform === "darwin" && "file://" === url.substring(0, 7)) {
       shell.showItemInFolder(url.substring(7, url.length));
-    }
-    else shell.openExternal(url);
+    } else shell.openExternal(url);
 
-    return { action: 'deny' };
+    return { action: "deny" };
   });
 
-  win.webContents.on('did-finish-load', () => {
-    win.webContents.send('message', {version: app.getVersion()});
+  win.webContents.on("did-finish-load", () => {
+    win.webContents.send("message", { version: app.getVersion() });
   });
 
-  win.on('close', (event) => {
+  win.on("close", (event) => {
     if (isProcessingMemories) {
       const choice = dialog.showMessageBoxSync(win, {
-        type: 'question',
-        buttons: ['Yes', 'No'],
+        type: "question",
+        buttons: ["Yes", "No"],
         defaultId: 1,
-        title: 'Confirm',
-        message: 'Are you sure you want to quit?',
-        detail: 'Your download will be interrupted.'
+        title: "Confirm",
+        message: "Are you sure you want to quit?",
+        detail: "Your download will be interrupted.",
       });
 
       if (choice === 1) {
@@ -60,54 +59,60 @@ const createWindow = () => {
 app.whenReady().then(() => {
   const window = createWindow();
 
-  app.on('window-all-closed', app.quit);
+  app.on("window-all-closed", app.quit);
 
-  app.on('activate', () => {
+  app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
 
-  ipcMain.on('beginDownload', (event, {input, output, options}) => {
-    if (isDebugging) console.log(`${input} selected as input\n${output} selected as download location`);
+  ipcMain.on("beginDownload", (event, { input, output, options }) => {
+    if (isDebugging)
+      console.log(
+        `${input} selected as input\n${output} selected as download location`
+      );
 
-    window.webContents.send('message', {message: 'Downloading memories'});
+    window.webContents.send("message", { message: "Downloading memories" });
     isProcessingMemories = true;
 
     downloadMemories(input, output, options, sendMessage)
-    .then(() => {
-      if (isDebugging) console.log('Download complete');
-    })
-    .catch((err) => {
-      if (isDebugging) console.log(`An error occurred while downloading memories. Error: ${err.message}`);
+      .then(() => {
+        if (isDebugging) console.log("Download complete");
+      })
+      .catch((err) => {
+        if (isDebugging)
+          console.log(
+            `An error occurred while downloading memories. Error: ${err.message}`
+          );
 
-      window.webContents.send('message', {
-        message: 'An unknown error occurred while processing your memories.<br />Please try again',
-        error: err
+        window.webContents.send("message", {
+          message:
+            "An unknown error occurred while processing your memories.<br />Please try again",
+          error: err,
+        });
+      })
+      .finally(() => {
+        isProcessingMemories = false;
       });
-    })
-    .finally(() => {
-      isProcessingMemories = false;
-    });
   });
 
-  ipcMain.on('chooseDownloadPath', () => {
-    dialog.showOpenDialog({
-      properties: ['openDirectory', 'createDirectory'],
-      buttonLabel: 'Select'
-    })
-    .then((res) => {
-      window.webContents.send('message', {
-        downloadLocation: path.resolve(res.filePaths[0])
+  ipcMain.on("chooseDownloadPath", () => {
+    dialog
+      .showOpenDialog({
+        properties: ["openDirectory", "createDirectory"],
+        buttonLabel: "Select",
+      })
+      .then((res) => {
+        window.webContents.send("message", {
+          downloadLocation: path.resolve(res.filePaths[0]),
+        });
       });
-
-    });
   });
 
-  ipcMain.on('reload', () => {
-    if (isDebugging) console.log('Reloading the window');
+  ipcMain.on("reload", () => {
+    if (isDebugging) console.log("Reloading the window");
 
     window.reload();
   });
 
-  const sendMessage = (data) => window.webContents.send('message', data);
+  const sendMessage = (data) => window.webContents.send("message", data);
 });
-
--- src/memoryDownloader.js
+++ src/memoryDownloader.js
@@ -1,36 +1,43 @@
-
 const {
   downloadPhotos,
-  downloadVideos
-} = require('./services/downloadServices.js');
+  downloadVideos,
+} = require("./services/downloadServices.js");
 const {
   initializeEnvironment,
   getMemoryDataFromJSON,
-  getOutputInfo
-} = require('./services/fileServices.js');
+  getOutputInfo,
+} = require("./services/fileServices.js");
 
 const isDebugging = process.env.DEBUG_MODE;
 
 const failedMemories = {
   photos: [],
   videos: [],
-  reAttempted: []
+  reAttempted: [],
 };
 
-const downloadMemories = async (filepath, outputDirectory, options, sendMessage) => {  
+const downloadMemories = async (
+  filepath,
+  outputDirectory,
+  options,
+  sendMessage
+) => {
   initializeEnvironment(filepath, outputDirectory);
-  
+
   const data = getMemoryDataFromJSON(filepath);
-  
-  if (!data['Saved Media']) {
+
+  if (!data["Saved Media"]) {
     sendMessage({
-      error: new Error('Invalid memories_history.json file provided. No "Saved Media" found.'),
-      message: 'Unable to parse the file you provided.<br />Please try uploading the <tt>memories_history.json</tt> file again.',
+      error: new Error(
+        'Invalid memories_history.json file provided. No "Saved Media" found.'
+      ),
+      message:
+        "Unable to parse the file you provided.<br />Please try uploading the <tt>memories_history.json</tt> file again.",
     });
     return;
   }
-  
-  const memories = data['Saved Media'].reverse();
+
+  const memories = data["Saved Media"].reverse();
 
   if (options.concurrent) {
     const sortedMemories = {};
@@ -41,17 +48,16 @@ const downloadMemories = async (filepath, outputDirectory, options, sendMessage)
       if (!(year in sortedMemories)) {
         sortedMemories[year] = {
           photos: [],
-          videos: []
+          videos: [],
         };
 
         for (let j = 0; j < memories.length; j++) {
           const memory = memories[j];
 
           if (year === memory.Date.substring(0, 4)) {
-            if (memory['Media Type'] === 'Image') {
+            if (memory["Media Type"] === "Image") {
               sortedMemories[year].photos.push(memory);
-            }
-            else {
+            } else {
               sortedMemories[year].videos.push(memory);
             }
           }
@@ -60,16 +66,30 @@ const downloadMemories = async (filepath, outputDirectory, options, sendMessage)
     }
 
     const tasks = [];
-    let photoCount = videoCount = total = 0;
+    let photoCount = 0;
+    let videoCount = 0;
+    let total = 0;
 
     for (const year in sortedMemories) {
       if (sortedMemories[year].photos) {
         photoCount += sortedMemories[year].photos.length;
-        tasks.push(downloadPhotos(sortedMemories[year].photos, failedMemories.photos, sendMessage));
+        tasks.push(
+          downloadPhotos(
+            sortedMemories[year].photos,
+            failedMemories.photos,
+            sendMessage
+          )
+        );
       }
       if (sortedMemories[year].videos) {
         videoCount += sortedMemories[year].videos.length;
-        tasks.push(downloadVideos(sortedMemories[year].videos, failedMemories.videos, sendMessage));
+        tasks.push(
+          downloadVideos(
+            sortedMemories[year].videos,
+            failedMemories.videos,
+            sendMessage
+          )
+        );
       }
     }
 
@@ -81,43 +101,65 @@ const downloadMemories = async (filepath, outputDirectory, options, sendMessage)
       photos: photoCount,
       videos: videoCount,
       totalMemories: total,
-      message: 'Downloading photos and videos'
+      message: "Downloading photos and videos",
     });
-
 
     await Promise.all(tasks);
 
     if (failedMemories.photos.length || failedMemories.videos.length) {
-      if (!(failedMemories.photos.length === photoCount && failedMemories.videos.length === videoCount)) {
+      if (
+        !(
+          failedMemories.photos.length === photoCount &&
+          failedMemories.videos.length === videoCount
+        )
+      ) {
         const reAttemptTasks = [];
-        if (isDebugging) console.log(`Re-attempting to download ${failedMemories.photos.length} photos and ${failedMemories.videos.length} videos`);
+        if (isDebugging)
+          console.log(
+            `Re-attempting to download ${failedMemories.photos.length} photos and ${failedMemories.videos.length} videos`
+          );
         sendMessage({ isReAttemptingFailedMemories: true });
-    
+
         if (failedMemories.photos.length) {
-          reAttemptTasks.push(downloadPhotos(failedMemories.photos, failedMemories.reAttempted, sendMessage));
+          reAttemptTasks.push(
+            downloadPhotos(
+              failedMemories.photos,
+              failedMemories.reAttempted,
+              sendMessage
+            )
+          );
         }
         if (failedMemories.videos.length) {
-          reAttemptTasks.push(downloadVideos(failedMemories.videos, failedMemories.reAttempted, sendMessage));
+          reAttemptTasks.push(
+            downloadVideos(
+              failedMemories.videos,
+              failedMemories.reAttempted,
+              sendMessage
+            )
+          );
         }
-        
-        sendMessage({message: `Re-attempting to download ${failedMemories.photos.length + failedMemories.videos.length} memories`});
+
+        sendMessage({
+          message: `Re-attempting to download ${
+            failedMemories.photos.length + failedMemories.videos.length
+          } memories`,
+        });
         await Promise.all(reAttemptTasks);
-      }
-      else {
-        failedMemories.reAttempted = failedMemories.photos.concat(failedMemories.videos);
+      } else {
+        failedMemories.reAttempted = failedMemories.photos.concat(
+          failedMemories.videos
+        );
       }
     }
-  }
-  else {
+  } else {
     let photos = [];
     let videos = [];
     let total = 0;
 
-    for (memory of memories) {
-      if (memory['Media Type'] === 'Image') {
+    for (const memory of memories) {
+      if (memory["Media Type"] === "Image") {
         photos.push(memory);
-      }
-      else {
+      } else {
         videos.push(memory);
       }
     }
@@ -128,38 +170,59 @@ const downloadMemories = async (filepath, outputDirectory, options, sendMessage)
     sendMessage({
       photos: photos.length,
       videos: videos.length,
-      totalMemories: total
+      totalMemories: total,
     });
 
     if (isDebugging) console.log(`Processing ${total} memories`);
 
     if (options.photos) {
-      sendMessage({message: 'Downloading photos'});
+      sendMessage({ message: "Downloading photos" });
       await downloadPhotos(photos, failedMemories.photos, sendMessage);
     }
-    
+
     if (options.videos) {
-      sendMessage({message: 'Downloading videos'});
+      sendMessage({ message: "Downloading videos" });
       await downloadVideos(videos, failedMemories.videos, sendMessage);
     }
 
     if (failedMemories.photos.length || failedMemories.videos.length) {
-      if (!(failedMemories.photos.length === photoCount && failedMemories.videos.length === videoCount)) {
-        if (isDebugging) console.log(`Re-attempting to download ${failedMemories.photos.length} photos and ${failedMemories.videos.length} videos`);
+      if (
+        !(
+          failedMemories.photos.length === photos.length &&
+          failedMemories.videos.length === videos.length
+        )
+      ) {
+        if (isDebugging)
+          console.log(
+            `Re-attempting to download ${failedMemories.photos.length} photos and ${failedMemories.videos.length} videos`
+          );
         sendMessage({ isReAttemptingFailedMemories: true });
-    
+
         if (failedMemories.photos.length) {
-          sendMessage({message: `Re-attempting to download ${failedMemories.photos.length} photos`});
-          await downloadPhotos(failedMemories.photos, failedMemories.reAttempted, sendMessage);
+          sendMessage({
+            message: `Re-attempting to download ${failedMemories.photos.length} photos`,
+          });
+          await downloadPhotos(
+            failedMemories.photos,
+            failedMemories.reAttempted,
+            sendMessage
+          );
         }
 
         if (failedMemories.videos.length) {
-          sendMessage({message: `Re-attempting to download ${failedMemories.videos.length} videos`});
-          await downloadVideos(failedMemories.videos, failedMemories.reAttempted, sendMessage);
+          sendMessage({
+            message: `Re-attempting to download ${failedMemories.videos.length} videos`,
+          });
+          await downloadVideos(
+            failedMemories.videos,
+            failedMemories.reAttempted,
+            sendMessage
+          );
         }
-      }
-      else {
-        failedMemories.reAttempted = failedMemories.photos.concat(failedMemories.videos);
+      } else {
+        failedMemories.reAttempted = failedMemories.photos.concat(
+          failedMemories.videos
+        );
       }
     }
   }
@@ -172,6 +235,5 @@ const downloadMemories = async (filepath, outputDirectory, options, sendMessage)
 
   sendMessage({ isComplete: true, ...downloadInfo });
 };
-
 
 module.exports = { downloadMemories };
